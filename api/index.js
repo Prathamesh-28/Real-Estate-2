db47a9000eb61d39713f0f90b21299fdd56d4022
Prathamesh-28 <pakothawade_b21@it.vjti.ac.in>
2024-03-28 18:56:12 +0530

    Conneted to Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to MongoDB!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 const app = express();
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
