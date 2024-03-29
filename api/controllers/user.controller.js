ac7179e79a840f30283f705eec42f151a92fd403
Prathamesh-28 <pakothawade_b21@it.vjti.ac.in>
2024-03-29 18:44:56 +0530

    created test API route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,8 @@
+const test = (req,res) =>{
+    res.json({
+        message: 'Hello World !!!',
+    });
+};
+module.exports = {
+    test
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRouter = require('./routes/user.route');
 
 dotenv.config();
 mongoose
@@ -15,3 +16,5 @@ const app = express();
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 });
+
+app.use("/api/user",userRouter);
