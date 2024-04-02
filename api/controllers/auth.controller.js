ac727c88cf543f62fa2e2fba3f62811e81927de5
Prathamesh-28 <pakothawade_b21@it.vjti.ac.in>
2024-04-02 21:30:23 +0530

    created signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model.js");
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
+const { errorHandler } = require("../utils/error.js");
+const jwt = require('jsonwebtoken');
 
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -12,6 +14,26 @@ const signup = async (req, res, next) => {
       next(error);
     }
 };
+
+const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try{
+      const validUser = await User.findOne({ email});
+      if(!validUser) return next(errorHandler(404, 'User not found'));
+      const validPassword = bcryptjs.compareSync(password,validUser.password);
+      if(!validPassword) return next(errorHandler(401, 'Wrong Credentials'));
+      const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+      const {password:pass, ...rest} = validUser._doc;
+      res
+        .cookie('access_token', token, { httpOnly: true})
+        .status(200)
+        .json(rest);
+    }
+    catch(error) {
+      next(error);
+    }
+};
+
 module.exports = {
-    signup
+    signup , signin
 };
